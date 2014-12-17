35f330f94f606ac7716edb1feafa0b09346a1bff
Wolfram Kriesing <wk@uxebu.com>
2014-12-17 16:13:07 +0100

    Ensure that FLOW* exports are configured.

--- bin/start-server.js
+++ bin/start-server.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 var app = require('../src/server/app.js');
 var port = parseInt(process.env.PORT, 10) || 9999;
+
+var isUserConfigured = 'FLOWUSER' in process.env;
+var isPasswordConfigured = 'FLOWPASS' in process.env;
+if (!isUserConfigured || !isPasswordConfigured) {
+  console.error('Please export FLOWUSER and FLOWPASS for the app to work.');
+  process.exit(-1);
+}
+
 var config = {
   flow: {
     user: process.env.FLOWUSER,
